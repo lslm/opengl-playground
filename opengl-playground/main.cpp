b0475b3add6ae872c2d837ccdb41c7976072a019
Lucas Santos <lucas.santos.silva2011@gmail.com>
2021-05-23 18:02:33 -0300

    Initial Commit

---
+++ opengl-playground/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  opengl-playground
+//
+//  Created by Lucas Santos on 23/05/21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
