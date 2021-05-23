bcadfa2da2bd8903da46a37b7c4b3b4d2604e3c8
Lucas Santos <lucas.santos.silva2011@gmail.com>
2021-05-23 18:30:43 -0300

    Init commit

--- opengl-playground/main.cpp
+++ opengl-playground/main.cpp
@@ -1,14 +1,37 @@
-//
-//  main.cpp
-//  opengl-playground
-//
-//  Created by Lucas Santos on 23/05/21.
-//
-
 #include <iostream>
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+int main() {
+    // Initializes GLFW to use its functions
+    glfwInit();
+    
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
+    
+    // Defines the OpenGL profile functions to be used:
+    // - Core (modern functions)
+    // - Compatibility (includes both modern and deprecated functions)
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+    
+    GLFWwindow * window = glfwCreateWindow(800, 600, "OpenGL Playground", nullptr, nullptr);
+    
+    if(window == nullptr) {
+        std::cout << "Failed to initilize GLFW" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+    
+    glfwMakeContextCurrent(window);
+    
+    while(!glfwWindowShouldClose(window)) {
+        glfwPollEvents();
+    }
+    
+    glfwDestroyWindow(window);
+    // Properly terminates GLFW;
+    glfwTerminate();
+    
     return 0;
 }
